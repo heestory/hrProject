6fd259c31a7358fc4b854bd0bb323857a4a2541c
정희 <jeonghui@jeonghuiui-MacBook-Pro.local>
2017-09-17 22:46:43 +0900

    backend

---
+++ hr_hire_register/src/main/java/com/hr/register/HrHireRegisterApplication.java
@@ -0,0 +1,12 @@
+package com.hr.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrHireRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrHireRegisterApplication.class, args);
+	}
+}
